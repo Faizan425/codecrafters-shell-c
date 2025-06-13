a9dfe5271a9de3b8ecd31b1aec0a9bc8863f425f
faizan <231001042@rajalakshmi.edu.in>
2025-06-13 17:02:13 +0530

    Done

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include<string.h>
 int main(int argc, char *argv[]) {
   // Flush after every printf
+  while(1){
   setbuf(stdout, NULL);
 
   // Uncomment this block to pass the first stage
@@ -13,5 +14,6 @@ int main(int argc, char *argv[]) {
   fgets(input, 100, stdin);
   input[strcspn(input, "\n")]= '\0';
   printf("%s: command not found\n", input);
+  }
   return 0;
 }
