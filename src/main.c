e0656d40027efbad6a6abc745113eedd93330476
faizan <231001042@rajalakshmi.edu.in>
2025-06-15 12:33:08 +0530

    Done

--- src/main.c
+++ src/main.c
@@ -4,8 +4,10 @@
 #include<ctype.h>
 #include<unistd.h>
 #include <limits.h>
+#include<sys/types.h>
+#include<sys/wait.h>
 
-
+int  get_arguments(char* inputs_copy,char **argument, int max_args);
 char* build_path(const char *path_token, const char *command,char* candidate_path, size_t candidate_path_size){
 	//if(strlen(path_token)+1+strlen(command)+1 > candidate_path_size){return NULL;}
 	//snprintf(candidate_path, candidate_path_size,"%s", path_token);
@@ -128,6 +130,59 @@ else{
 }
 
 }
+
+int get_arguments(char* inputs_copy, char** argument,int max_args){
+	int argc = 0;
+	char *p;
+	while(*p && isspace((unsigned char)*p)) p++;
+	while(*p !='\0' && argc < max_args){
+		argument[argc++] = p;
+		while(*p && !isspace((unsigned char)*p)) p++;
+		if(*p=='\0') break;
+		*p='\0';
+		p++;
+		while(*p && isspace((unsigned char)*p)) p++;
+
+	
+	}
+	argument[argc]=NULL;
+
+
+
+
+}
+void  execute_custom(char input[]){
+	char *argument[100];
+	char *inputs_copy=strdup(input);
+		if(!inputs_copy){
+			perror("strdup");
+			return;
+		}
+		int argc=get_arguments(inputs_copy,argument,100);
+		if(argc==0){
+			free(inputs_copy);
+			return;
+		}
+		pid_t pid=fork();
+		if(pid<0){
+			perror("fork");
+			return;
+		}
+		if(pid==0){
+			execvp(argument[0], argument);
+			perror(argument[0]);
+			return;
+		}
+		else{
+			int status;
+			if(waitpid(pid, &status,0)<0){
+				perror("waitpid");
+			}
+			free(inputs_copy);
+		}
+			
+}
+
 char* remove_command_and_get_string(char input[] ){
 	int i=skip_leading_spaces(input);
 	while(input[i] != '\0' && !isspace((unsigned char)input[i])){
@@ -161,7 +216,7 @@ void execute_command(char *command,char input[]){
 		execute_type(input);
 	}
 	else{
-		printf("%s: command not found\n",input);
+		execute_custom(input);
 	}
 }
 
